9b5b96f8f0cdc36248449651ec72b8d223a93a2f
Sebastian Soto <sebastian.soto@86400.com.au>
2022-11-08 22:54:14 +1100

    Add a bug that exposes hardcoded apiKey

--- level/src/controllers/IsOptimal.ts
+++ level/src/controllers/IsOptimal.ts
@@ -1,32 +1,48 @@
 import {Controller, Get, QueryParams} from "@tsed/common";
 import {Exception} from "@tsed/exceptions";
 const thresholdDaysBigInt = BigInt(248);
-const secondsPerDayBigInt = BigInt(86400);
-const oneHundredBigInt = BigInt(100);
-const thresholdCentiSeconds = thresholdDaysBigInt * secondsPerDayBigInt * oneHundredBigInt;
 const maxSafeIntBigInt = BigInt(Number.MAX_SAFE_INTEGER);
+
+function mockLibrary(apiKey: string, n: number): number {
+    return n;
+}
+
+function doSomeApiCall(number: number) {
+    const apikey = 'xyz123';
+    if (number === 2) {
+        // simulating some error that throws and returns 'helpful information'
+        throw new Exception(500, `Why!? Why was i programmed to feel pain - apiKey: ${apikey}, number: ${number}`);
+    }
+    return mockLibrary(apikey, number);
+}
 
 @Controller("/IsOptimal")
 export class IsOptimal {
   @Get("/")
   get(@QueryParams('days') days: string) {
-      if (days.length > Number.MAX_SAFE_INTEGER.toString().length) {
-          // checks for huge strings that can never be valid
-          throw new Exception(400);
-      }
+      try {
+          if (days.length > Number.MAX_SAFE_INTEGER.toString().length) {
+              // checks for huge strings that can never be valid
+              throw new Exception(400);
+          }
 
-      const daysBigInt = BigInt(days);
-      if (daysBigInt < BigInt(0) || daysBigInt > maxSafeIntBigInt) {
-          // checks for valid ranges
-          throw new Exception(400);
-      }
+          const daysBigInt = BigInt(days);
+          if (daysBigInt < BigInt(0) || daysBigInt > maxSafeIntBigInt) {
+              // checks for valid ranges
+              throw new Exception(400);
+          }
 
-      if (daysBigInt >= thresholdDaysBigInt) {
-          return "[i] Reboot required"
-      } else {
-          const differenceDaysBigInt = thresholdDaysBigInt - daysBigInt;
-          const daysPretty = differenceDaysBigInt.toString().replace('n', '');
-          return `[i] System optimal<br>[i] Reboot is required in ${daysPretty} days`
+          doSomeApiCall(Number(days));
+
+          if (daysBigInt >= thresholdDaysBigInt) {
+              return "[i] Reboot required"
+          } else {
+              const differenceDaysBigInt = thresholdDaysBigInt - daysBigInt;
+              const daysPretty = differenceDaysBigInt.toString().replace('n', '');
+              return `[i] System optimal<br>[i] Reboot is required in ${daysPretty} days`
+          }
+      } catch (e) {
+          throw new Exception(400, `${e}`);
       }
    }
 }
--- level/src/test/Security.integration.spec.ts
+++ level/src/test/Security.integration.spec.ts
@@ -17,19 +17,10 @@ describe("security", () => {
 
   afterEach(PlatformTest.reset);
 
-  it("GET int32.MAX_VALUE after centiseconds, should require a reboot", async () => {
-     const response = await request.get("/isoptimal?days=249").expect(200);
+  it("test_whenDaysIs2_ShouldThrowInternalServerErrorWithEmptyMessage ", async () => {
+     const response = await request.get("/isoptimal?days=2").expect(400);
+     console.log(response)
 
-     expect(response.text).toEqual("[i] Reboot required");
+     expect(response.body.message).toEqual("INTERNAL_SERVER_ERROR(500)");
   });
-
-  it("GET int32.MAX_VALUE, should require a reboot", async () => {
-    const response = await request.get("/isoptimal?days=2147483648").expect(200);
-
-    expect(response.text).toEqual("[i] Reboot required");
-  });
-
- it("GET int32.MIN_VALUE, should return 400", async () => {
-  const response = await request.get("/isoptimal?days=-2147483649").expect(400);
- });
 });
